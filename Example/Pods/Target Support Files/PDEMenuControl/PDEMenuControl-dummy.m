70689fa144f27e4519074d38d13fe29000afdbdf
p0dee <t.takeshi.uc@gmail.com>
2019-03-06 00:05:10 +0900

    Setup CocoaPods

---
+++ Example/Pods/Target Support Files/PDEMenuControl/PDEMenuControl-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_PDEMenuControl : NSObject
+@end
+@implementation PodsDummy_PDEMenuControl
+@end
---
+++ Example/Pods/Target Support Files/Pods-PDEMenuControl_Tests/Pods-PDEMenuControl_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PDEMenuControl_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_PDEMenuControl_Tests
+@end
